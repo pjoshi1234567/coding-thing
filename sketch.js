86a5cd94e767ae6b7dcb5144bf901dbb426df313
pjoshi1234567 <113735168+pjoshi1234567@users.noreply.github.com>
2022-09-16 16:55:08 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var sprite1
+function setup() {
+  createCanvas(400,400);
+sprite1=createSprite(200,200,20,20)
+sprite1.shapeColor="blue"
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites()
+
+}
+
+
+
+
